4a3b16081b7a4243406d7370ae57bc8d0a0463d6
jjannix <jannik.heer@gmail.com>
2023-12-10 19:58:25 +0100

    fixed and improved verification

--- commands/moderation/verifyUser.js
+++ commands/moderation/verifyUser.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { memberRoleId, logChannel } = require('../../configuration/config.json')
+const { memberRoleId, logChannel } = require('../../configuration/config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,29 +9,52 @@ module.exports = {
         .addBooleanOption(option => option.setName('confirm').setDescription('Confirm the verification').setRequired(true)),
 
     async execute(interaction) {
-        const user = interaction.options.getMember('user');
-        const confirm = interaction.options.getBoolean('confirm');
-        const channel = interaction.guild.channels.cache.get(logChannel);
-        if (user.id != interaction.user.id) {
+        try {
+            const user = interaction.options.getMember('user');
+            const confirm = interaction.options.getBoolean('confirm');
+            const channel = interaction.guild.channels.cache.get(logChannel);
 
-            if (confirm === true) {
-                const role = interaction.guild.roles.cache.get(memberRoleId);
+            const verificationEmbed = new EmbedBuilder()
+                .setColor('#B9FF97')
+                .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL() })
+                .setTitle('User Verified')
+                .setDescription(`User **${user.user.username}** has been verified by **${interaction.user.username}**.`)
+                .addFields(
+                    { name: 'Verified at', value: new Date().toLocaleString() },
+                )
+                .setTimestamp()
+                .setFooter({ text: '© @jnk 2023' });
+            
+            if (user.id !== interaction.user.id) {
+                if (confirm === true) {
+                    const role = interaction.guild.roles.cache.get(memberRoleId);
 
-                if (user.roles.cache.has(role.id)) {
-                    return interaction.reply({ content: 'User already verified!', ephemeral: true });
+                    if (!role) {
+                        return interaction.reply({ content: 'Role not found. Please check your configuration.', ephemeral: true });
+                    }
+
+                    if (user.roles.cache.has(role.id)) {
+                        return interaction.reply({ content: 'User already verified!', ephemeral: true });
+                    }
+
+                    try {
+                        await user.roles.add(role);
+                        await user.send(`Congratulations! You have been verified by ${interaction.user.username}! `);
+                        channel.send({ embeds: [verificationEmbed] });
+                        return interaction.reply({ content: 'User verified!', ephemeral: true });
+                    } catch (error) {
+                        console.error(error);
+                        return interaction.reply({ content: 'Error adding role to user.', ephemeral: true });
+                    }
+                } else {
+                    return interaction.reply({ content: 'Verification not confirmed.', ephemeral: true });
                 }
-                try {
-                    await user.roles.add(role);
-                    await user.send(`Congratulations! You have been verified by ${interaction.user.username}! `);
-                    channel.send(`User ${user.user.username} has been verified by ${interaction.user.username} at ${new Date().toLocaleString()}.`);
-                    return interaction.reply({ content: 'User verified!', ephemeral: true });
-                } catch (error) {
-                    console.error(error);
-                    return interaction.reply({ content: 'Error adding role to user.', ephemeral: true });
-                }
+            } else {
+                return interaction.reply({ content: 'You cannot verify yourself!', ephemeral: true });
             }
-        } else {
-            return interaction.reply({ content: 'You cannot verify yourself!', ephemeral: true });
+        } catch (error) {
+            console.error(error);
+            return interaction.reply({ content: 'An error occurred during verification.', ephemeral: true });
         }
     },
 };
