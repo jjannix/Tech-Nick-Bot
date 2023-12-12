8b090c39495392998e78935dd8c9787749413fe8
jjannix <jannik.heer@gmail.com>
2023-12-12 22:39:53 +0100

    added embeds and logging to ban command

--- commands/moderation/banUser.js
+++ commands/moderation/banUser.js
@@ -1,22 +1,48 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { logChannelId } = require('../../configuration/config.json');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ban')
-		.setDescription('Bans the targeted User!')
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Bans the targeted User!')
         .addUserOption(option => option.setName('target').setDescription('The user to ban').setRequired(true))
-        .addStringOption(option => option.setName('reason').setDescription('The reason for the ban').setRequired(true))
-        .addBooleanOption(option => option.setName('sure').setDescription('Are you sure you want to unban this user?').setRequired(true)),
-	async execute(interaction) {
-        const user = interaction.options.getUser('target');
-        const guild = interaction.guild;
-        const banEmbed = new EmbedBuilder()
+        .addBooleanOption(option => option.setName('sure').setDescription('Are you sure you want to unban this user?').setRequired(true))
+        .addStringOption(option => option.setName('reason').setDescription('The reason for the ban').setRequired(false)),
         
+    
+    async execute(interaction) {
+        const user = interaction.options.getUser('target');
+        const reasonGiven = interaction.options.getString('reason');
+        const guild = interaction.guild;
+        const channel = await interaction.guild.channels.cache.get(logChannelId);
+        const reason = reasonGiven || 'No reason given';
+
+        const banEmbed = new EmbedBuilder()
+            .setColor('#BD3E3C')
+            .setAuthor({ name: interaction.user?.tag, iconURL: interaction.user?.displayAvatarURL() })
+            .setTitle('User Banned')
+            .setDescription(`User **${user.username}** has been banned by **${interaction.user?.username || 'Unknown User'}** :wave:.`)
+            .setTimestamp()
+            .setFooter({ text: '© @jnk 2023' });   
+
+        const banLog = new EmbedBuilder()
+            .setColor('#BD3E3C')
+            .setAuthor({ name: interaction.user?.tag, iconURL: interaction.user?.displayAvatarURL() })
+            .setTitle('User Banned')
+            .setDescription(`User **${user.username}** has been banned by **${interaction.user?.username || 'Unknown User'}**.`)
+            .addFields(
+                { name: 'Reason', value: reason },
+                { name: 'Banned at', value: new Date().toLocaleString() },
+            )
+            .setTimestamp()
+            .setFooter({ text: '© @jnk 2023' });
+
         if (interaction.options.getBoolean('sure') === true) {
-            guild.members.ban(user);
-		    return interaction.reply({content: 'Banned user: ' + user.username + '!', ephemeral: true });
+            guild.members.ban(user, { reason })
+            channel.send({ embeds: [banLog] });
+            interaction.reply({ embeds: [banEmbed] });
         } else {
             return interaction.reply('Crisis averted! I did not perform this action!');
         }
-	},
+    },
 };
