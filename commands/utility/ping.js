e17af4a6fd72bf354c9e8ce704ecdf3c91c12107
jjannix <jannik.heer@gmail.com>
2024-05-01 01:20:31 +0200

    added latency meassuring to ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { isHighLatency } = require('../functions/highLatency');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		return interaction.reply('Pong!');
-	},
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction) {
+        const reply = await interaction.reply({ content: "Pinging...", fetchReply: true });
+        const ping = reply.createdTimestamp - interaction.createdTimestamp;
+        const channelCache = interaction.client.channels.cache;
+        if (await isHighLatency(ping, interaction, channelCache)) {
+            console.log(`High latency detected for ping command: ${ping}ms`);
+        }
+        interaction.editReply(`Pong! Bot Latency: ${ping}ms`);
+    },
 };
